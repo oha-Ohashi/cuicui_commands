05540de58bc85b36066f2e08b1c90fbfa8546420
Ohashi_uhbie <tmatomatox1@gmail.com>
2022-03-25 20:17:41 +0900

    hello

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/hello', methods=['GET', 'POST'])
 def hello_heroku():
     return "Hello Heroku from flask!"
 
